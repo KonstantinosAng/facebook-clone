cd1e26b3d4afd7f3fdc5e14162d451152921646e
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-26 22:06:05 +0200

    :zap: styling header

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header.js';
 function App() {
   return (
     <div className="App">
-      <h1> Facebook </h1>
       <Header />
     </div>
   );
--- facebook/src/components/Header.js
+++ facebook/src/components/Header.js
@@ -1,20 +1,66 @@
 import React from 'react'
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
+import HomeIcon from '@material-ui/icons/Home';
+import FlagIcon from '@material-ui/icons/Flag';
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import StorefrontIcon from '@material-ui/icons/Storefront';
+import SupervisedUserCircleIcon from '@material-ui/icons/SupervisedUserCircle';
+import AddIcon from '@material-ui/icons/Add';
+import ForumIcon from '@material-ui/icons/Forum';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { Avatar, IconButton } from '@material-ui/core';
 
 function Header() {
   return (
     <div className="header__root">
-      <div className="header_left">
+      <div className="header__left">
         <img
+         onClick={()=>window.location.reload()}
+         className="header__left__image"
          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
          alt="Facebook logo"
-         />
+        />
+        <div className="header__input">
+          <SearchIcon />
+          <input type="text" />
+        </div>
       </div>
-      <div className="header_middle">
-
+      <div className="header__middle">
+        <div className="header__option">
+          <HomeIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <FlagIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SubscriptionsIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <StorefrontIcon fontSize="large" />
+        </div>
+        <div className="header__option">
+          <SupervisedUserCircleIcon fontSize="large" />
+        </div>
       </div>
-      <div className="header_right">
-
+      <div className="header__right">
+        <div className="header__info">
+          <Avatar />
+          <h4>Constantine Angelopoulos</h4>
+        </div>
+        <IconButton>
+          <AddIcon />
+        </IconButton>
+        <IconButton>
+          <ForumIcon />
+        </IconButton>
+        <IconButton>
+          <NotificationsActiveIcon />
+        </IconButton>
+        <IconButton>
+          <ExpandMoreIcon />
+        </IconButton>
       </div>
     </div>
   )
