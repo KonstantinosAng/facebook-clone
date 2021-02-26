bcfee940cfbdcbb5069d50a324a8492d0a4e4c1b
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-26 21:05:40 +0200

    :zap: added material ui

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './components/Header.js';
 
 function App() {
   return (
     <div className="App">
       <h1> Facebook </h1>
+      <Header />
     </div>
   );
 }
---
+++ facebook/src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header__root">
+      <div className="header_left">
+        <img
+         src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png"
+         alt="Facebook logo"
+         />
+      </div>
+      <div className="header_middle">
+
+      </div>
+      <div className="header_right">
+
+      </div>
+    </div>
+  )
+}
+
+export default Header
