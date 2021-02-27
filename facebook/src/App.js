1d2caa09dda10375ab32981d3d31d072bbc49153
KonstantinosAng <kwstantinos.agelopoulos@outlook.com>
2021-02-27 22:31:59 +0200

    :sparkles: build complete

--- facebook/src/App.js
+++ facebook/src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import Header from './components/Header.js';
 import Sidebar from './components/Sidebar.js';
 import Feed from './components/Feed.js';
+import Widgets from './components/Widgets.js';
 
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? <h1>Login</h1> : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ facebook/src/components/Widgets.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Widgets.css';
+
+function Widgets() {
+  return (
+    <div className="widgets__root">
+      <iframe
+      src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Fspacextechnologies%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover-false&show_facepile=true&appId"
+      width="340"
+      height="100%"
+      style={{border: "none", overflow: "hidden"}}
+      scrolling="no"
+      frameborder="0"
+      allowTransparency="true"
+      allow="encrypted-media"
+      />
+    </div>
+  )
+}
+
+export default Widgets
